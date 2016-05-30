abe7778be678fc5dfb165b642ec668ea7a2c5478
Gert Dewit <gert@hobbiton.be>
2016-05-31 01:04:45 +0200

    drive also external LED

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 SDCC=sdcc-sdcc
 SDLD=sdcc-sdld
-OBJECTS=blinky.ihx
+BLINKY=blinky
+OBJECTS=$(BLINKY).ihx
 FAC_DEF=factory_defaults.bin
 MODEL=stm8s103f3
 PROGRAMMER=stlinkv2
@@ -12,8 +13,8 @@ all: $(OBJECTS)
 clean:
 	rm -f *.asm *.cdb *.ihx *.lk *.lst *.map *.rel *.rst *.sym $(FAC_DEF)
 
-flash: $(OBJECT).ihx
-	stm8flash -c $(PROGRAMMER) -p $(MODEL) -w $(OBJECT).ihx
+flash: $(BLINKY).ihx
+	stm8flash -c $(PROGRAMMER) -p $(MODEL) -w $<
 
 reset: $(FAC_DEF)
 	stm8flash -c $(PROGRAMMER) -p $(MODEL) -s opt -w $(FAC_DEF)
--- blinky.c
+++ blinky.c
@@ -1,16 +1,29 @@
-#define PB_ODR *(unsigned char*)0x500F
-#define PB_IDR *(unsigned char*)0x5010
-#define PB_DDR *(unsigned char*)0x5011
-#define PB_CR1 *(unsigned char*)0x5012
-#define PB_CR2 *(unsigned char*)0x5013
+/**
+* blinky: blink the LED on the board connected to B5
+*         and an external LED if connected to D4
+*         using a dumb wait routine
+**/
+#define PB_ODR *(unsigned char*)0x5005  // Port B data output latch register
+#define PB_DDR *(unsigned char*)0x5007  // Port B data direction register
+#define PB_CR1 *(unsigned char*)0x5008  // Port B control register 1
+#define PD_ODR *(unsigned char*)0x500F  // Port D data output latch register
+#define PD_DDR *(unsigned char*)0x5011  // Port D data direction register
+#define PD_CR1 *(unsigned char*)0x5012  // Port D control register 1
+#define CLK_CKDIVR *(unsigned char*)0x50C6  // Clock divider 
 
 int main() {
-	int d;
-	PB_DDR = 0x20;
+	long d;
+	CLK_CKDIVR = 0x00;
+	PB_DDR = 0x20; // Port 5 : 00010000 = 0x20
 	PB_CR1 = 0x20;
+	PD_DDR = 0x10; // Port 4 : 00001000 = 0x10
+	PD_CR1 = 0x10;
 
 	do {
-		PB_ODR ^= 0x20;
-		for(d = 0; d < 29000; d++) {}
+		PB_ODR ^= 0x20; // Flip the active bits
+		PD_ODR ^= 0x10; 
+		// Dumb loop
+		for(d = 0; d < 80000; d++) {
+		}
 	} while(1);
 }
